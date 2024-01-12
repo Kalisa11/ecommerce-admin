1fb49337837ab18337dc3764bd72b9236918b0a6
Kalisa <95277748+Kalisa11@users.noreply.github.com>
2024-01-12 12:15:34 +0200

    categories feature (#5)

--- app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { useState } from "react";
 import axios from "axios";
-import toast from "react-hot-toast";
+import { useState } from "react";
 import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+import { toast } from "react-hot-toast";
 import { useParams, useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -12,76 +14,72 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { BillboardColums } from "./columns";
-import { Button } from "@/components/ui/button";
+
+import { CategoriesColumns } from "./columns";
 import AlertModal from "@/components/modals/alert-modal";
 
 interface CellActionProps {
-  data: BillboardColums;
+  data: CategoriesColumns;
 }
-const CellAction = ({ data }: CellActionProps) => {
 
+export const CellAction: React.FC<CellActionProps> = ({ data }) => {
   const router = useRouter();
   const params = useParams();
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
-  const onCopy = (id: string) => {
-    navigator.clipboard.writeText(id);
-    toast.success("Copied to clipboard");
+
+  const onConfirm = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/categories/${data.id}`);
+      toast.success("Category deleted.");
+      router.refresh();
+    } catch (error) {
+      toast.error("Error deleting category");
+    } finally {
+      setOpen(false);
+      setLoading(false);
+    }
   };
 
-  const deleteBillboard = async () => {
-    setLoading(true);
-    try {
-      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
-      router.refresh();
-      toast.success("Billboard deleted");
-    } catch (error) {
-      toast.error("Ensure you have no products or categories");
-    } finally {
-      setLoading(false);
-      setOpen(false);
-    }
+  const onCopy = (id: string) => {
+    navigator.clipboard.writeText(id);
+    toast.success("Category ID copied to clipboard.");
   };
 
   return (
     <>
       <AlertModal
-        item="Billboard"
         isOpen={open}
+        item="Category"
         onClose={() => setOpen(false)}
-        onConfirm={deleteBillboard}
+        onConfirm={onConfirm}
         loading={loading}
       />
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant={"ghost"} className="h-8 w-8 p-0">
+          <Button variant="ghost" className="h-8 w-8 p-0">
             <span className="sr-only">Open menu</span>
-            <MoreHorizontal className="w-4 h-4" />
+            <MoreHorizontal className="h-4 w-4" />
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
           <DropdownMenuLabel>Actions</DropdownMenuLabel>
           <DropdownMenuItem onClick={() => onCopy(data.id)}>
-            <Copy className="mr-2 w-4 h-4" />
-            Copy ID
+            <Copy className="mr-2 h-4 w-4" /> Copy Id
           </DropdownMenuItem>
           <DropdownMenuItem
             onClick={() =>
-              router.push(`/${params.storeId}/billboards/${data.id}`)
+              router.push(`/${params.storeId}/categories/${data.id}`)
             }
           >
-            <Edit className="mr-2 w-4 h-4" />
-            Edit
+            <Edit className="mr-2 h-4 w-4" /> Edit
           </DropdownMenuItem>
           <DropdownMenuItem onClick={() => setOpen(true)}>
-            <Trash className="mr-2 w-4 h-4" />
-            Delete
+            <Trash className="mr-2 h-4 w-4" /> Delete
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
     </>
   );
 };
-
-export default CellAction;
