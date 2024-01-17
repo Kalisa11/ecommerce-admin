c4c22f5cdb7abd126dfda8aa65d09554aa55ecff
Kalisa <honorekals5@gmail.com>
2024-01-17 18:00:08 +0200

    dark mode & dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import { ModalProvider } from "@/providers/modal-provider";
 import { ToastProvider } from "@/providers/toast-provider";
+import { ThemeProvider } from "@/providers/theme-provider";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,9 +22,16 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <ToastProvider />
-        <ModalProvider />
-        <body className={inter.className}>{children}</body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ToastProvider />
+          <ModalProvider />
+          <body className={inter.className}>{children}</body>
+        </ThemeProvider>
       </html>
     </ClerkProvider>
   );
